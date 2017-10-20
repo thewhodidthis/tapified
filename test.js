1e93e802df2606396404ff40578599d86644838c
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-10-20 07:27:50 +0200

    test: cleanup

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// Stolen from tapjs/tapsert
+// Adapted from tapjs/tapsert
 const { execFile } = require('child_process')
 const { execPath } = process
 
@@ -15,48 +15,51 @@ assert(assert.equal, 'assert.equal exists')
 
 assert('truthy, not a description')
 
-assert.equal('auto description', 'auto description')
+assert.equal('skip description', 'skip description')
 assert.equal(typeof assert.strictEqual, 'function', 'assert.strictEqual is a function')
 
 assert.doesNotThrow(() => {
   assert.throws(() => {
     throw Error('expected!')
   }, /expected/, 'supports assert.throws')
-}, 'nested asserts are weird')
+}, Error, 'are nested asserts really that weird?')
 
 const ego = require.resolve('./')
 
 execFile(execPath, [ego], (error, stdout, stderr) => {
   assertHeader(stdout, 'no assertions')
-  assert(error, 'not unlike tapsert no assertions run is no failure')
 
   assert.equal(stderr, '', 'does not write to stderr')
-  assert.equal(stdout.includes('Bail out!'), false, 'does not exit prematurely')
 
+  assert.equal(stdout.includes('Bail out!'), false, 'does not exit prematurely')
+  assert.equal(error, null, 'unlike tapsert no assertions ran is no failure')
   assert(stdout.includes('# tests 0'), 'includes bill')
 })
 
-const tip = require.resolve('./example/dump.js')
+const bad = require.resolve('./example/bail.js')
+
+execFile(execPath, [bad], (error, stdout, stderr) => {
+  assertHeader(stdout, 'example bails (bad)')
+
+  assert.notEqual(stderr, '', 'does not clear stderr')
+
+  assert(stdout.includes('Bail out!'), 'does exit prematurely')
+  assert(error, 'does exit with a non-zero error code')
+
+  assert.equal(stdout.includes('# tests 0'), false, 'excludes bill')
+})
+
+const tip = require.resolve('./example')
 
 execFile(execPath, [tip], (error, stdout, stderr) => {
-  assertHeader(stdout, 'failing example')
+  assertHeader(stdout, 'example fails')
 
-  assert(error, 'example fails and exits with non-zero error code')
   assert.equal(stderr, '', 'does not write to stderr')
 
   assert(stdout.includes('1..6'), 'has TAP plan')
   assert(stdout.includes('# tests 6'), 'has 6 tests total')
   assert(stdout.includes('# pass  5'), 'has 5 passing tests')
   assert(stdout.includes('# fail  1'), 'has 1 failing test')
-})
 
-const bad = require.resolve('./example/bail.js')
-
-execFile(execPath, [bad], (error, stdout, stderr) => {
-  assertHeader(stdout, 'bad file')
-  assert(error, 'bad file does exit with a non-zero error code')
-  assert(stdout.includes('Bail out!'), 'does exit prematurely')
-
-  assert.notEqual(stderr, '', 'does not clear stderr')
-  assert(!stdout.includes('# tests 0'), 'excludes bill')
+  assert.equal(error, null, 'unlike tapsert mess not with exit code on failure')
 })
